089d41218b6d504cddfe77b2608abc070579558c
Ashton Storks <ashton@pistudios.net>
2015-12-11 18:19:38 -0700

    First fucking commit

---
+++ src/DemoInfoWs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+
+namespace DemoInfoWs
+{
+    public class Program
+    {
+        public static HttpServer HttpServer { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            HttpServer = new HttpServer(8086);
+        }
+    }
+}
---
+++ src/DemoInfoWs/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DemoInfoWs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoInfoWs")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ff46ed3-ad19-4563-936d-0ae78abcbf65")]
