008ab5373bc390412302a632a1d17c7a6eeb260b
Ashton Storks <ashton@pistudios.net>
2015-12-13 11:56:06 -0700

    Unbrokeded

--- src/DemoGo.Parser/Program.cs
+++ src/DemoGo.Parser/Program.cs
@@ -8,19 +8,34 @@ namespace DemoGo.Parser
 {
     public class Program
     {
+        private static int roundNumber = 0;
+
         public static void Main(string[] args)
         {
             DemoInfo.DemoParser parser = new DemoInfo.DemoParser(File.OpenRead("./fuck.dem"));
             parser.PlayerKilled += Parser_PlayerKilled;
+            parser.PlayerBind += Parser_PlayerBind;
+            parser.RoundStart += Parser_RoundStart;
             parser.ParseHeader();
+            Console.WriteLine("{0}, {1}", parser.TickRate, parser.Header);
             parser.ParseToEnd();
-            Console.WriteLine(parser.TickRate);
             Console.ReadLine();
+        }
+
+        private static void Parser_RoundStart(object sender, DemoInfo.RoundStartedEventArgs e)
+        {
+            roundNumber++;
+            Console.WriteLine("Round {0} started", roundNumber);
+        }
+
+        private static void Parser_PlayerBind(object sender, DemoInfo.PlayerBindEventArgs e)
+        {
+            Console.WriteLine("{0} connected", e.Player.Name);
         }
 
         private static void Parser_PlayerKilled(object sender, DemoInfo.PlayerKilledEventArgs e)
         {
-            Console.WriteLine("{0} killed {1} with {2}", e.Killer.Name, e.Victim.Name, e.Weapon.Weapon);
+            Console.WriteLine("{0} killed {1} with {2}", e.Killer?.Name ?? "Bot", e.Victim?.Name ?? "Bot", e.Weapon?.Weapon);
         }
     }
 }
